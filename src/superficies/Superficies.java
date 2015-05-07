3d04cf1aa986c5bc0f52e958ec2986a587dcd483
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-05-07 10:11:39 +0200

    Clase Superficie retocada mediante el modelo MVC!!!

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -6,6 +6,10 @@ import javax.swing.JOptionPane;
 /**
  * Examen de MVC 3ª Avaliación
  *
+ * * Descripcion : Este programa, el cual está configurado sobre el modelo MVC,
+ * pedirá el nombre de una figura para calcular su área (Cuadrado, Rectángulo,
+ * írculo o Triángulo)
+ *
  * @author damian
  */
 public class Superficies {
@@ -16,103 +20,94 @@ public class Superficies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        //Constructores de la clase Modelo y Vista
+        Modelo contenedor = new Modelo();
+        Vista vis = new Vista();
+
         String shapeType;
 
         sc = new Scanner(System.in);
 
-        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
+        //Preguntamos que área se desea obtener
+        System.out.println("Cual es el area que desea obtener? (Cuadrado, Circulo, Triangulo, Rectangulo):");
         shapeType = sc.nextLine();
-        System.out.println("You said: " + shapeType);
+        //Se envía la elección de shapeType a la clase Modelo.
+        contenedor.setShapeType(shapeType);
+        //Se enseña la elección escogida 
+        System.out.println("Tu eliges: " + shapeType);
 
-        if (shapeType.equals("square")) {
+        //Mediante un "if" se pocederá a calcular el área de la figura escogida
+        if (shapeType.equals("Cuadrado")) {
             //add area calculations for square
             float squareArea;
             float sideLength;
 
-            System.out.println("what is the side length?");
+            //Preguntamos el lado del cuadrado
+            System.out.println("Cual es el area del cuadrado?");
             sideLength = sc.nextFloat();
+            //Mandamos el lado a la clase Modelo
+            contenedor.setSideLength(sideLength);
             squareArea = sideLength * sideLength;
-            System.out.println("The area for your square: " + squareArea);
+            //Mandamos el area a la clase Modelo
+            contenedor.setSquareArea(squareArea);
+            vis.imprimir(contenedor);
 
         }
-        if (shapeType.equals("rectangle")) {
+        if (shapeType.equals("Rectangulo")) {
             //add area calculations for rectangle here
             float sideLength;
             float sideHeight;
             float rectangleArea;
 
-            System.out.println("what is the rectangles width?");
+            //Preguntamos la altura del rectángulo
+            System.out.println("Cual es la altura del rectangulo?");
             sideLength = sc.nextFloat();
-            System.out.println("What is the rectangles height?");
+            contenedor.setSideLength(sideLength);
+            //Preguntamos el base del rectángulo
+            System.out.println("Cual es la base del rectangulo");
             sideHeight = sc.nextFloat();
+            contenedor.setSideHeight(sideHeight);
+            //Mandamos los datos a la clase Modelo
             rectangleArea = sideLength * sideHeight;
-            System.out.println("The area for your rectangle is: " + rectangleArea);
+            contenedor.setRectangleArea(rectangleArea);
+            vis.imprimir(contenedor);
 
         }
-        if (shapeType.equals("triangle")) {
+        if (shapeType.equals("triangulo")) {
             //add area calculations for triangle here
             float baseLength;
             float height;
             float triangleArea;
-
-            System.out.println("What is the base length of the triangle?");
+            //Preguntamos la base del triangulo
+            System.out.println("Cual el la base del triangulo");
             baseLength = sc.nextFloat();
-            System.out.println("What is the height of the triangle?");
+            contenedor.setBaseLength(baseLength);
+            //Preguntamos la altura del triangulo
+            System.out.println("Cual es la altura de triangulo?");
             height = sc.nextFloat();
+            contenedor.setHeight(height);
+            //Mandamos los datos a la clase Modelo
             triangleArea = (float) (0.5 * baseLength * height);
-            System.out.println("Your triangles area is: " + triangleArea);
+            contenedor.setTriangleArea(triangleArea);
+            vis.imprimir(contenedor);
 
         }
-        if (shapeType.equals("circle")) {
+        if (shapeType.equals("Circulo")) {
             //add area calculations for a circle here
             float radius;
             float circleArea;
 
-            System.out.println("What is the radius of the circle?");
+            //Preguntamos el radio del circulo
+            System.out.println("Cual es el radio del circulo?");
             radius = sc.nextFloat();
+            contenedor.setRadius(radius);
             circleArea = radius * radius;
+            //Mandamos los datos a la clase Modelo
             circleArea = (float) (3.14159265 * circleArea);
-            System.out.println("Your Circles area is " + circleArea);
-
+            contenedor.setCircleArea(circleArea);
+            vis.imprimir(contenedor);
         }
-
-    }
-    
-
-    public static void realizarOperacion(int op) {
-
-        float resultado = 0;
-
-        float areaCuadrado = 0;
-        float lado = 0;
-        float altura = 0;
-        float areaRectangulo = 0;
-        float base = 0;
-        float areaTriangulo = 0;
-        float radio = 0;
-        float areaCirculo = 0;
-
-        switch (op) {
-
-            case 1:
-                resultado = (lado * lado);
-                break;
-            case 2:
-                resultado = (base * altura);
-                break;
-            case 3:
-                resultado = ((base * altura) / 2);
-                break;
-            case 4:
-                resultado = (radio * radio);
-                break;
-            case 5:
-                System.exit(op);
-            default:
-                JOptionPane.showMessageDialog(null, "Introduzca una opcion valida");
-                break;
-        }
-        return resultado;
 
     }
 }
